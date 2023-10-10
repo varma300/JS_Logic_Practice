e4a51bc46216de0e79231b27a3db18bad58bb812
Rahul Varma <rahulvarma300@gmail.com>
2023-10-10 22:15:22 +0530

    CamelCase

--- test.js
+++ test.js
@@ -1,43 +1,33 @@
-/*Capitalise the first letter of each sentence of a paragraph.(Score 3)
-Input:- let str = "javascript is the world’s most popular programming language. it is the programming langu
-age of the web. and it is easy to learn.";
-Output:- Javascript is the world’s most popular programming language. It is the programming language of
-the web. And it is easy to learn.*/
+// camelCase
 
-let str = `javascript is the world’s most popular programming language. it is the programming language of the web. and it is easy to learn.`;
+let word = 'my name is khan'
 
-// split the word using split method from each space, then it will convert to an array
-let arr = str.split(' ')
-// console.log(arr);
+// for converting camelCase split the string and create an new array
 
-// create a empty array for saving final output
- let final_output = []
+let arr = word.split(' ')
+// console.log(arr); // checking the output of the arr
 
-// itrate each element of array using for loop
 
-for(i=0;i<arr.length;i++){
-   // arr[i] itrate the elemens of array
-   // arr[i][0] itrate the 0th position/first letter of the array
-   // toUpperCase() is the metheod for convert the letter to upprcase 
-   // save the output in to a variable
+// converting 0 th index string in lowercase
+let f_word = arr[0].toLowerCase();
+// console.log(f_word);// checking the output of the f_word
 
-   let F_letter = arr[i][0].toUpperCase()
+// itrate from 1st indux element using for loop
+// create a variable for save output of for loop
+let out = [];
 
-   // slice method using for create a new string/array from exsisting string/array
-   // slice contains two elements in the paranthisis first one for starting idux, second for stopping indux + 1 
-   // arr[i].slice(1,) itrate new string from each elemnt of array from first indux
-   // save the vale to a variable
+for (i=1;i<arr.length;i++){
+    // find the first letter and convert to upper case
+ let Cap_let = arr[i][0].toUpperCase();
+//  console.log(Cap_let);
 
-   let rest_words = arr[i].slice(1,)
-   // both values concat using + method and save to output
-
-   let output = F_letter+rest_words
-
-   // add each value to empty array using push methed
-
-   final_output.push(output)
-  
+    // take rest letters and confirm it is lowercase
+    let rest_ltr = arr[i].slice(1,)
+    // console.log(rest_ltr);
+    let str = Cap_let + rest_ltr;
+    out.push(str)
 }
-// join methed will convert the array to string and replace all metehed will use to replace comma to space
-console.log(final_output.join(',').replaceAll(',',' '));
+// console.log(out);
+let final = f_word+out.join().replaceAll(',','');
 
+console.log(final);
