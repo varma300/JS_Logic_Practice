f5ccece4130e21a6c81060d277c778eed77a798a
Rahul Varma <rahulvarma300@gmail.com>
2023-10-11 23:37:19 +0530

    Flick Switch

--- test.js
+++ test.js
@@ -1,33 +1,31 @@
-// camelCase
-
-let word = 'my name is khan'
-
-// for converting camelCase split the string and create an new array
-
-let arr = word.split(' ')
-// console.log(arr); // checking the output of the arr
+/*Create a function that always returns true for every item in a given list. 
+However, if an element is the word "flick", switch to always returning the opposite boolean value.*/
 
 
-// converting 0 th index string in lowercase
-let f_word = arr[0].toLowerCase();
-// console.log(f_word);// checking the output of the f_word
+function flick_test(arr){ 
+    // cretate a varible assinge true as value
+    let flicker = true;
+    // create an emplty array
+    let out = [];
+    
+    // itrate the array using for loop
+    for(i=0;i<arr.length;i++){
+        // check if arra elemt is equal to 'flick' using if condition
+        if(arr[i]=='flick'){
+            // if the condition pass, change the value of flicker to opposit boolean (true becom false)
+            // once it change, the new vale will reasign in to the variable till next 'flick' comes
+            flicker = !flicker;
+            // push the output in to the array
+           out.push(flicker)
 
-// itrate from 1st indux element using for loop
-// create a variable for save output of for loop
-let out = [];
-
-for (i=1;i<arr.length;i++){
-    // find the first letter and convert to upper case
- let Cap_let = arr[i][0].toUpperCase();
-//  console.log(Cap_let);
-
-    // take rest letters and confirm it is lowercase
-    let rest_ltr = arr[i].slice(1,)
-    // console.log(rest_ltr);
-    let str = Cap_let + rest_ltr;
-    out.push(str)
+        }
+        else{
+            // if not equal to 'flick' the value add exsisting boolean value add to array
+            out.push(flicker)
+        }
+    }
+    // print the output
+    console.log(out);
 }
-// console.log(out);
-let final = f_word+out.join().replaceAll(',','');
 
-console.log(final);
+flick_test([1,2,3,4,'flick',1,2,3,4,'flick',1,2,3])
