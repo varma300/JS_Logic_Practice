da8722b0eb03e43461b1523b59e58be048e1c2a2
Rahul Varma <rahulvarma300@gmail.com>
2023-10-13 17:07:43 +0530

    Area or Perimeter

--- test.js
+++ test.js
@@ -1,31 +1,17 @@
-/*Create a function that always returns true for every item in a given list. 
-However, if an element is the word "flick", switch to always returning the opposite boolean value.*/
+/* You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
 
+Example(Input1, Input2 --> Output):
 
-function flick_test(arr){ 
-    // cretate a varible assinge true as value
-    let flicker = true;
-    // create an emplty array
-    let out = [];
-    
-    // itrate the array using for loop
-    for(i=0;i<arr.length;i++){
-        // check if arra elemt is equal to 'flick' using if condition
-        if(arr[i]=='flick'){
-            // if the condition pass, change the value of flicker to opposit boolean (true becom false)
-            // once it change, the new vale will reasign in to the variable till next 'flick' comes
-            flicker = !flicker;
-            // push the output in to the array
-           out.push(flicker)
+6, 10 --> 32
+3, 3 --> 9*/
 
-        }
-        else{
-            // if not equal to 'flick' the value add exsisting boolean value add to array
-            out.push(flicker)
-        }
+function areaOrPerimeter(l,w){
+    // if squer
+    if (l == w){
+        return l*w
+    }else{
+        //if rectangle
+        return (2(l+w))
     }
-    // print the output
-    console.log(out);
 }
-
-flick_test([1,2,3,4,'flick',1,2,3,4,'flick',1,2,3])
