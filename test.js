ce0806c15bf5d1072b6d61fe63d5dc6b60f0bf7d
Rahul Varma <rahulvarma300@gmail.com>
2023-10-14 18:28:06 +0530

    negative number

--- test.js
+++ test.js
@@ -1,17 +1,13 @@
-/* You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
-If it is a square, return its area. If it is a rectangle, return its perimeter.
+/*In this kata you will create a function that takes a list of non-negative integers 
+and strings and returns a new list with the strings filtered out.
+*/
 
-Example(Input1, Input2 --> Output):
-
-6, 10 --> 32
-3, 3 --> 9*/
-
-function areaOrPerimeter(l,w){
-    // if squer
-    if (l == w){
-        return l*w
-    }else{
-        //if rectangle
-        return (2(l+w))
+function filter_list(l) {
+    let new_array = [];
+    for(e of l){
+        if(e )
+        return new_array.push(e)
     }
-}
+  }
+
+  
