03035726554757a240e28c0732f758ace4bfbb04
Rahul Varma <rahulvarma300@gmail.com>
2023-10-18 21:52:10 +0530

    Get Middle

--- test.js
+++ test.js
@@ -1,25 +1,30 @@
+/*
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 
-// Define the API endpoint URL
-const apiUrl = 'https://dummyjson.com/quotes';
+#Examples:
 
-// Function to fetch data from the API
-function fetchData() {
-  fetch(apiUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      // Handle the fetched data here
-      console.log('Fetched data:', data);
-    })
-    .catch((error) => {
-      // Handle errors
-      console.error('Error:', error);
-    });
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+*/
+
+function getMiddle(s)
+{
+  let len = s.length
+  let cent = len/2
+if(len % 2 == 0){
+  console.log(typeof s[cent -1]+s[cent]);
+}else{
+  console.log(s[cent-0.5]);
+}
 }
 
-// Call the fetchData function to initiate the API request
-fetchData();
+
+getMiddle("test")
+getMiddle("testing")
+getMiddle("middle") 
+getMiddle("A") 
