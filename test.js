3d508afdbebd357c37c57318c140428ae978d3b3
Rahul Varma <rahulvarma300@gmail.com>
2023-10-27 13:06:56 +0530

    Q2

--- test.js
+++ test.js
@@ -1,16 +1,29 @@
 /*
-You have an array of elements, and you need to perform two operations on each element:
 
-1. Calculate the square of each element in the array.
-2. Multiply all the squared values together to get a final result.
+## Question 2:
 
-Let's assume we have the following array:*/
+When coloring a striped pattern, you may start by coloring each square *sequentially*, meaning you spend time needing to *switch coloring pencils*.
 
-let input = [1,2,3,4];
+Create a function where given an *array of colors* `cols`, return how long it takes to color the whole pattern. Note the following times:
 
-let test = (e)=>{
-let pow = e.map(e => Math.pow(e,2)).reduce((t,c)=>t+c)
-return pow  
+- It takes **1 second** to *switch between pencils*.
+- It takes **2 seconds** to *color a square*.
+
+color_pattern_times(["Red", "Blue", "Red", "Blue", "Red"]) ➞ 14
+
+// There are 5 colors so it takes 2 seconds to color each one (2 x 5 = 10).
+// You need to switch the pencils 4 times and it takes 1 second to switch (1 x 4 = 4).
+// 10 + 4 = 14
+*/
+
+function color_pattern_times(e){
+    let len = e.length;
+    let out =(len*2) + (len-1);
+    console.log(out);
+
 }
 
-console.log(test(input));
+
+color_pattern_times(["Red", "Blue", "Red", "Blue", "Red"])
+
+
