5f12b6889a532c1950db48f3c0995c9a2c33e71f
Rahul Varma <rahulvarma300@gmail.com>
2023-11-20 23:00:23 +0530

    API

---
+++ featch.js
@@ -0,0 +1,24 @@
+fetch('https://fakestoreapi.com/products').then((data)=>{
+    // console.log(data);
+    return data.json()
+}).then((compleatData)=>{
+    // console.log(compleatData);
+
+    let data1 = '';
+    compleatData.map((values)=>{
+        data1 += ` <div class="cards">
+        <h1 class="title">${values.title}</h1>
+        <img src=${values.image}
+            alt="img" class="images">
+
+        <p>${values.description}</p>
+        <p class="category">${values.category}</p>
+        <p class="price">${values.price}</p>
+
+
+    </div>`
+    })
+    document.getElementById("card").innerHTML = data1
+}).catch((err)=>{
+    console.log(err);
+})
