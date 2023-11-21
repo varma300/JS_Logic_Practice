4fd4cd443495c212d4023b3c19210ed1c5b0b7c5
Rahul Varma <rahulvarma300@gmail.com>
2023-11-21 17:38:09 +0530

    API using Async Await

--- featch.js
+++ featch.js
@@ -1,7 +1,10 @@
-fetch('https://fakestoreapi.com/products').then((data)=>{
+/*
+fetch('https://fakestoreapi.com/products')
+.then((data)=>{
     // console.log(data);
     return data.json()
-}).then((compleatData)=>{
+})
+.then((compleatData)=>{
     // console.log(compleatData);
 
     let data1 = '';
@@ -19,6 +22,34 @@ fetch('https://fakestoreapi.com/products').then((data)=>{
     </div>`
     })
     document.getElementById("card").innerHTML = data1
-}).catch((err)=>{
+})
+.catch((err)=>{
     console.log(err);
 })
+*/
+
+async function featchData(){
+    try{
+        let responce = await fetch('https://fakestoreapi.com/products');
+        let data = await responce.json();
+        let data1
+         data.map((values)=>{
+            data1 += ` <div class="cards">
+            <h1 class="title">${values.title}</h1>
+            <img src=${values.image}
+                alt="img" class="images">
+    
+            <p>${values.description}</p>
+            <p class="category">${values.category}</p>
+            <p class="price">${values.price}</p>
+    
+    
+        </div>`
+        })
+        document.getElementById("card").innerHTML = data1
+    }catch(error){
+        document.write(error)
+    }
+}
+
+featchData()
