e23da144f333035d26a6dcd56943d48965b7f7dc
Rahul Varma <rahulvarma300@gmail.com>
2023-11-22 12:25:13 +0530

    String Methods

--- test.js
+++ test.js
@@ -1,23 +1,77 @@
-// Global Scope
-const pie = 3.14;
+
+/*
+A string in JavaScript is a data type that represents text.
+ It is a sequence of characters enclosed in single or double quotes.
+
+*/
+
+let str = `A string in JavaScript is a data type that represents text.
+It is a sequence of characters enclosed in single or double quotes.`;
+
+// length
+let len = str.length
+console.log(len);
+
+//slice
+
+let sls = str.slice(12,22);
+console.log(sls);
+
+//replace / replaceAll
+
+let rps = str.replace('is','was')
+let rpsal = str.replaceAll('is', 'HAVE')
+console.log(rpsal);
+
+//toUpperCase / toLowercase
+
+let up = str.toUpperCase()
+console.log(up);
+
+let loc = up.toLowerCase()
+console.log(loc);
+
+//concat
+
+let x = 'hello';
+let y = 'hi';
+let out = y.concat(x)
+console.log(out);
+
+let out2 = x+ ' ' +y;
+console.log(out2);
+
+// trim
+
+let wsp = '  hi  hello  ';
+console.log(wsp);
+let trm = wsp.trim();
+console.log(trm);
+
+let tst = wsp.trimStart();
+console.log(tst);
+
+let ted = wsp.trimEnd();
+console.log(ted);
+
+//padStart / padEnd
+
+let a = '00';
+
+let o = a.padStart(5,'*')
+console.log(o);
+
+let oe = a.padEnd(3,'#')
+console.log(oe);
+
+//caheAt
+
+let name = 'my nAme is khan';
+let cA = name.charAt(5);
+console.log(cA);
 
 
-function fn1(){
-    console.log(pie);//3.14
+//charCodeAt 
 
-    // function scope
-    const age = 32;
-    console.log(age); //32
-}
-
-// block scope
-
-if (true){
-const fullName = 'Rahul Varma TK';
-console.log(fullName); //Rahul Varma TK
-
-}
-
-console.log(pie);  //3.14
-console.log(age); // RefferenceError : age is not defined
-console.log(fullName); // RefferenceError : fullName is not defined
+let CCA = name.charCodeAt(4);
+console.log(CCA);
