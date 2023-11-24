aca431b2f9d31604cca07c5789f6c3d2eecfc26f
Rahul Varma <rahulvarma300@gmail.com>
2023-11-25 01:11:24 +0530

    Javascript Object

--- test.js
+++ test.js
@@ -1,58 +1,50 @@
-
-
 /*
-A JavaScript array is a collection of values, each with an index. 
-Arrays are used to store multiple values of the same type.
-Array elements are accessed using their indices, which are non-negative integers.
-The first element has an index of 0, the second element has an index of 1, 
-and so on. JavaScript arrays are dynamic, 
- which means that they can grow and shrink as needed.
-  */
+In JavaScript, an object is a collection of key-value pairs. 
+The keys are called properties, and the values are called values. 
+Objects are used to store data and to organize code. They are essential for
+ creating complex and reusable components in JavaScript applications. */
 
- let arr = [1,2,true,false,'string'];
- console.log(arr);
- 
- // length
- 
- let len = arr.length;
- console.log(len);
- 
- // toString
- let str = arr.toString();
- console.log( str);
- 
- // pop / push
- 
- let POP = arr.pop();
- console.log(arr);
- console.log(POP);
- 
- arr.push('name')
- console.log(arr);
- 
- 
- // shift / unshift
- 
- arr.shift();
- console.log(arr);
- 
- arr.unshift(45);
- console.log(arr);
- 
- // join
- 
- str = arr.join(' and ')
- console.log( str);
- 
- // splice
- 
- arr.splice(1,0, 99, 'text');
- console.log(arr);
- 
- // slice
- 
- let arr1 = arr.slice(2,5);
- console.log(arr1);
- 
- 
- 
+// Object literals
+
+let person = {
+    name : 'rahul',
+    age : 35,
+    place : 'Chennai',
+    about : function (){
+        console.log('my name is ' + this.name  + ' and I am ' + this.age + 'years old.');
+    }
+}
+
+console.log(person);
+
+
+// function Constructor
+
+// function Person(name, age, place){
+//     this.name = name;
+//     this.age = age;
+//     this.place = place;
+// }
+
+
+// let obj = new Person('Rahul',35,'Chennai');
+
+// console.log(obj.place);
+
+
+// person.qlf = 'BCOM'
+
+// console.log(person);
+
+// delete person.age;
+
+// console.log(person);
+
+
+// method
+
+person.about()
+
+
+
+console.log(this === window);
