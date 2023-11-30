4be03054b02129a1fe7e718bd65b6d9ec5ffc692
Rahul Varma <rahulvarma300@gmail.com>
2023-11-30 15:14:20 +0530

    form submit using DOM

--- test.js
+++ test.js
@@ -1,50 +1,16 @@
-/*
-In JavaScript, an object is a collection of key-value pairs. 
-The keys are called properties, and the values are called values. 
-Objects are used to store data and to organize code. They are essential for
- creating complex and reusable components in JavaScript applications. */
+function form_submit(e){
+    e.preventDefault();
+    let name = document.getElementById('name').value;
+    let phone = document.getElementById('number').value;
+    let mail = document.getElementById('email').value;
 
-// Object literals
+    document.getElementById('nh').innerHTML = 'Name';
+    document.getElementById('pH').innerHTML = 'Phone Number';
+    document.getElementById('eh').innerHTML = 'email'
+    
+    document.getElementById('nm').innerHTML = `${name}`;
+    document.getElementById('ph').innerHTML = `${phone}`;
+    document.getElementById('ml').innerHTML = `${mail}` ;
+};
 
-let person = {
-    name : 'rahul',
-    age : 35,
-    place : 'Chennai',
-    about : function (){
-        console.log('my name is ' + this.name  + ' and I am ' + this.age + 'years old.');
-    }
-}
-
-console.log(person);
-
-
-// function Constructor
-
-// function Person(name, age, place){
-//     this.name = name;
-//     this.age = age;
-//     this.place = place;
-// }
-
-
-// let obj = new Person('Rahul',35,'Chennai');
-
-// console.log(obj.place);
-
-
-// person.qlf = 'BCOM'
-
-// console.log(person);
-
-// delete person.age;
-
-// console.log(person);
-
-
-// method
-
-person.about()
-
-
-
-console.log(this === window);
+document.getElementById('form').addEventListener('submit',form_submit);
