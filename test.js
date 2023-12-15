d2bd5f6278bb0ee5c4d454f16058158271bed7cd
Rahul Varma <rahulvarma300@gmail.com>
2023-12-15 19:41:12 +0530

    Practice

--- test.js
+++ test.js
@@ -1,26 +1,25 @@
-`Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+`Example 1:
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.`
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.`
+
+var isPalindrome = function(x) {
+let str = x.toString()
+let str1 = str.split('').reverse().join('')
+return( str1===str);
+
+};
 
 
-
-let nums = [3,3]
-let target = 6
-
-var twoSum = function(nums, target) {
-  let out ;
-  for(i=0;i<nums.length;i++){
-    for(j=i+1;j<nums.length;j++){
-      if(nums[i]+nums[j] == target){
-      
-        out = [i,j]
-      }
-      
-    }
-    
-  }
-  return out
-    }
-
-
-console.log(twoSum(nums, target));
+console.log(isPalindrome(121))
