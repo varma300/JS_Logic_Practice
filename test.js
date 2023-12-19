b0560bfbd086e217c12301fe7f05df7f5ccd7b87
Rahul Varma <rahulvarma300@gmail.com>
2023-12-20 01:21:50 +0530

    Callback Hell

--- test.js
+++ test.js
@@ -1,33 +1,33 @@
-`The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
+`Callback hell in JavaScript refers to the situation where you have multiple nested callback functions, making your code incredibly hard to read, debug, and maintain. It's like a pyramid of doom, built layer upon layer of callbacks, leading to messy and tangled logic.
 
-For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
-Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference between pairs (nums[w], nums[x]) and (nums[y], nums[z]) is maximized.
+Here's how it typically happens:
 
-Return the maximum such product difference.
+You initiate an asynchronous operation using a function that takes a callback function as an argument. This callback will be called with the result of the operation when it finishes.
+Inside the callback, you may need to perform another asynchronous operation, again involving a callback.
+This nesting can continue, adding more and more layers of callbacks, each dependent on the previous one finishing.
+Why is this problematic?
 
- 
-
-Example 1:
-
-Input: nums = [5,6,2,7,4]
-Output: 34
-Explanation: We can choose indices 1 and 3 for the first pair (6, 7) and indices 2 and 4 for the second pair (2, 4).
-The product difference is (6 * 7) - (2 * 4) = 34.
-Example 2:
-
-Input: nums = [4,2,5,9,7,4,8]
-Output: 64
-Explanation: We can choose indices 3 and 6 for the first pair (9, 8) and indices 1 and 5 for the second pair (2, 4).
-The product difference is (9 * 8) - (2 * 4) = 64.
+Indentation explosion: The code becomes deeply indented, making it visually confusing and difficult to follow the flow of execution.
+Error handling becomes a nightmare: Debugging errors within nested callbacks is challenging, as the error source might be buried several layers deep.
+Readability suffers: The logic becomes convoluted and hard to understand, especially for those unfamiliar with the code.
 `
-let nums = [4,2,5,9,7,4,8]
 
-var maxProductDifference = function(nums) {
-    let sort_data = nums.sort(function(a,b){return a-b})
-    let a = sort_data.slice(-2,).reduce((a,b)=>a*b) ;
-    let b = sort_data.slice(0,2).reduce((a,b)=>a*b);
-    return(a-b);
-};
+function getUserID(userName, callback) {
+    setTimeout(() => callback(1234), 1000)
+}
+
+function getEmailAdd(userID, callback) {
+    setTimeout(() => callback('user@testmail.com'), 500)
+}
 
 
-maxProductDifference(nums)
+function logEmailId(email) {
+    console.log(`user email : ${email}`);
+}
+
+
+getUserID('Sree', (userID) => {
+    getEmailAdd(userID, (email) => {
+        logEmailId(email)
+    })
+})
