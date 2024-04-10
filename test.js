4a5ad602a3158d2c8bab56442894496f6ad00547
varma300 <rahulvarma300@gmail.com>
2024-04-10 10:16:14 +0530

    Mask Card

--- test.js
+++ test.js
@@ -1,3 +1,32 @@
+// card mask
+
+let cardNumber = '3214-2654-6549-4568';
+let cardLen = cardNumber.length;
+let maskCard = cardNumber.slice(-4).padStart(cardLen,'*');
+
+console.log(maskCard);   //***************4568
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 `
 Promises are like little IOUs in JavaScript. They represent the eventual completion (or failure) of an asynchronous operation and its resulting value. Instead of waiting for the operation to finish before moving on, promises let you register functions to be called later, when the result is available. This makes your code cleaner and more readable.
 
@@ -13,22 +42,22 @@ You can't directly access the result of a promise. Instead, you use its then and
 `
 
 
-function getUserId(userName){
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>resolve(1234),1000)
-    })
-}
+// function getUserId(userName){
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>resolve(1234),1000)
+//     })
+// }
 
-function getEmailAdd(userID){
-return new Promise((resolve, reject)=>{
-    setTimeout(()=>resolve('user@testmail.com'), 500)
-})
-}
+// function getEmailAdd(userID){
+// return new Promise((resolve, reject)=>{
+//     setTimeout(()=>resolve('user@testmail.com'), 500)
+// })
+// }
 
-function logEmailId(email){
-    console.log(`user mail id : ${email}`);
+// function logEmailId(email){
+//     console.log(`user mail id : ${email}`);
     
-}
+// }
 
 
 
@@ -39,14 +68,14 @@ function logEmailId(email){
 // .catch(error =>console.error(error))
 
 
-async function main(){
-    try{
-        const userID = await getUserId('SRee')
-        const email = await getEmailAdd(userID)
-        logEmailId(email)
-    }catch(error){
-        console.error(error);
-    }
-}
+// async function main(){
+//     try{
+//         const userID = await getUserId('SRee')
+//         const email = await getEmailAdd(userID)
+//         logEmailId(email)
+//     }catch(error){
+//         console.error(error);
+//     }
+// }
 
-main()
+// main()
